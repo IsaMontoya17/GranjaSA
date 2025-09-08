1dadc7704a7524b1a93ebb5083bec81e2486bd57
Sara Castañeda <saracastanedaecheverri@gmail.com>
2025-09-08 12:09:04 -0500

    Add(web): alimentacion register form

--- frontend-granja/src/App.jsx
+++ frontend-granja/src/App.jsx
@@ -1,6 +1,7 @@
 import Layout from './components/layout/Layout'
 import AdminPage from './pages/adminPage/AdminPage'
 import NotFound from './pages/notFound/NotFound'
+import AlimentacionForm from "./pages/alimentacion/AlimentacionForm";
 import { Routes, Route } from 'react-router-dom'
 
 import './App.css'
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<AdminPage />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/alimentacion/nuevo" element={<AlimentacionForm />} />
       </Route>
     </Routes>
     </>
